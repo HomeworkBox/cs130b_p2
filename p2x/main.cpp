e7376d53c0dd7026cdc65a8bb73fbb73ccae5211
tyler@mbp <ouyang@cs.ucsb.edu>
2015-02-25 22:37:20 -0800

    Initial Commit

---
+++ p2x/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p2x
+//
+//  Created by Tyler Weimin Ouyang on 2/25/15.
+//  Copyright (c) 2015 Golden. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
